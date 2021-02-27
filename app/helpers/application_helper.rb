a11854401913c6ed33ed49c8784e6988a324e330
Your Name <basaijapascal9@gmail.com>
2021-02-27 15:56:48 +0300

    fix rubocop

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,9 +2,10 @@ module ApplicationHelper
   def upvote_or_downvote(article)
     upvoted = Vote.find_by(article: article, user: current_user)
     if upvoted
-      link_to('Downvote!', article_vote_path(id: upvoted.id, article_id: article.id), method: :delete, class:"article-title")
+      link_to('Downvote!', article_vote_path(id: upvoted.id, article_id: article.id), method: :delete,
+                                                                                      class: 'article-title')
     else
-      link_to('Upvote!', article_votes_path(article_id: article.id), method: :post, class:"article-title")
+      link_to('Upvote!', article_votes_path(article_id: article.id), method: :post, class: 'article-title')
     end
   end
 
